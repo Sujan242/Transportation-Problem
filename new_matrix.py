ec9d9c7a5fbde4b84a948ccd73169b24eb9b91a6
Sujan242 <sujanreddy242@gmail.com>
2020-05-22 22:37:22 +0530

    updated table 3A

--- new_matrix.py
+++ new_matrix.py
@@ -2,14 +2,16 @@ import pandas as pd
 import ast
 data=pd.read_excel('Results.xlsx')
 # 34,35
-xa=[20,21,22,24,25,26,27,28,31,32,34]
+# xa=[20,21,22,24,25,26,27,28,31,32,34]
 ya=[]
+xa=[]
 for i in range(20,36):
 	# if i not in xa:
 	# 	ya.append(i)
+	xa.append(i)
 	ya.append(i)
 # print(len(ya))
-ol= [[0 for i in range(11)] for j in range(16)]
+ol= [[0 for i in range(16)] for j in range(16)]
 # xa=[20]
 for i in range(1,641):
 	j=0
